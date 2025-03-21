b445ebd5c9af72b45c4414e0248ddad230a7523c
hscdexolusion <tayworgbalestevestan@gmail.com>
2025-03-21 02:47:56 +0000

    blog page no easy sha

--- src/Blog/components/otherTrends.jsx
+++ src/Blog/components/otherTrends.jsx
@@ -1,7 +1,234 @@
 import React from 'react'
+import "./otherTrends.css"
+import Buying1 from "/src/Blog/images/Buying1.png"
+import Buying2 from "/src/Blog/images/Buying2.png"
+import Buying3 from "/src/Blog/images/Buying3.png"
+import Selling1 from "/src/Blog/images/Selling1.png"
+import Selling2 from "/src/Blog/images/Selling2.png"
+import Selling3 from "/src/Blog/images/Selling3.png"
+import Guide1 from "/src/Blog/images/Guide1.png"
+import Guide2 from "/src/Blog/images/Guide2.png"
+import Guide3 from "/src/Blog/images/Guide3.png"
+import Renting1 from "/src/Blog/images/Renting1.png"
+import Renting2 from "/src/Blog/images/Renting2.png"
+import Renting3 from "/src/Blog/images/Renting3.png"
 
 export const OtherTrends = () => {
-  return (
-    <div>otherTrends page</div>
-  )
+  return <div className="elements">
+    {/*Buying */}
+  <h1 className="h1">Buying</h1>
+ <div className="row-one">
+   <div className="buying">
+     <img src={Buying1} alt="row one image one"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   <div className="buying">
+     <img src={Buying2} alt="row one image two"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   
+   <div className="buying">
+     <img src={Buying3} alt="row one image three"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+ 
+
+   </div>
+   
+          {/*Selling */}
+   <h1 className="h1">Selling</h1>
+ <div className="row-two">
+   <div className="selling">
+     <img src={Selling1} alt="row two image one"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   <div className="selling">
+     <img src={Selling2} alt="row two image two"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   
+   <div className="selling">
+     <img src={Selling3} alt="row two image three"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+ 
+
+   </div>
+   
+      {/*Guide */}
+   <h1 className="h1">Guide</h1>
+ <div className="row-three">
+   <div className="Guide">
+     <img src={Guide1} alt="row three image one"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   <div className="Guide">
+     <img src={Guide2} alt="row three image two"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   
+   <div className="Guide">
+     <img src={Guide3} alt="row three image three"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+ 
+
+   </div>
+  
+        {/*Renting */}
+   <h1 className="h1">Renting</h1>
+ <div className="row-four">
+   <div className="Renting">
+     <img src={Renting1} alt="row four image one"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   <div className="Renting">
+     <img src={Renting2} alt="row four image two"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+   
+   <div className="Renting">
+     <img src={Renting3} alt="row four image three"  className="Image"/>
+     <div className="text">
+                     <div className="ImageTitle"> 
+                         Buying
+                     </div>
+                         <div className="ImageData"> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
+                         </div>
+                             <div className="ImageAuthor">
+                                 Wade warren
+                             </div>
+                 </div>
+   </div>
+
+ 
+
+   </div>
+ 
+
+
+ </div>
+
 }
