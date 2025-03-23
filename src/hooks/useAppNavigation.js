b14790342ef9c669272fc1cc8c93dfc219fd191c
Reindolf Appiagyei <125794743+reinakwasi@users.noreply.github.com>
2025-03-22 23:14:50 -0700

    OtherTrends (#12)
    * trends
    
    * color scheme
    
    * chnaged from npm to yarn and updated code
    
    * listings
    
    * otherTrends

--- src/hooks/useAppNavigation.js
+++ src/hooks/useAppNavigation.js
@@ -1,8 +1,7 @@
-import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useCallback } from "react"; // Importing React hooks
+import { useNavigate } from "react-router-dom"; // Importing useNavigate from React Router
 
 export const useAppNavigation = (route, from) => {
-    const navigate = useNavigate();
-    return useCallback(() => navigate(route, { state: { from } }), [navigate, route])
+    const navigate = useNavigate(); // Getting navigate function from React Router
+    return useCallback(() => navigate(route, { state: { from } }), [navigate, route]); // Returning a callback to navigate
 };
-
